d34486e49ff3ab52769eea254e1acd690d6971a8
That-one-coder-0 <alej10kleon@gmail.com>
2025-06-14 08:07:15 -0400

    update

--- js/tag.js
+++ js/tag.js
@@ -1101,7 +1101,7 @@ function multi1() {
     document.getElementById('settingIcon').style.display = 'none';
     document.getElementById('shopbutton').style.display = 'none';
     //document.getElementById('Shop').style.backgroundColor = color.menus;
-    document.getElementById('selectCharcter').style.display = 'block';
+    document.getElementById('selectCharcter').style.display = 'block'; 
     document.getElementById('selectCharcter1').style.display = 'block';
     document.getElementById('selectCharcter2').style.display = 'block';
     document.getElementById('selectCharcter1').style.position = 'absolute';
