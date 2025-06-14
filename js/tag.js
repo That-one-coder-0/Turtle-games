590616bfe5512901d47717ab790c7ea833e205c0
That-one-coder-0 <alej10kleon@gmail.com>
2025-06-13 20:22:33 -0400

    update

--- js/tag.js
+++ js/tag.js
@@ -1101,7 +1101,7 @@ function multi1() {
     document.getElementById('settingIcon').style.display = 'none';
     document.getElementById('shopbutton').style.display = 'none';
     //document.getElementById('Shop').style.backgroundColor = color.menus;
-    document.getElementById('selectCharcter').style.display = 'block';
+    document.getElementById('selectCharcter1').style.display = 'block';
     document.getElementById('selectCharcter2').style.display = 'block';
     document.getElementById('selectCharcter1').style.position = 'absolute';
     document.getElementById('selectCharcter1').style.left = '2%'; 
@@ -1119,7 +1119,6 @@ function multi2() {
     document.getElementById('settingIcon').style.display = 'none';
     document.getElementById('shopbutton').style.display = 'none';
     //document.getElementById('Shop').style.backgroundColor = color.menus;
-    document.getElementById('selectCharcter').style.display = 'block';
     document.getElementById('selectCharcter1').style.display = 'block'; 
     document.getElementById('selectCharcter2').style.display = 'block'; 
     document.getElementById('selectCharcter3').style.display = 'block'; 
